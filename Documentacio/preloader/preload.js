2030a0f06995fdd9ac20a750e396d20c0925889e
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-11-02 12:26:42 +0100

    dd

---
+++ Documentacio/preloader/preload.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const scene = document.querySelector('a-scene');
+    const splash = document.querySelector('#splash');
+    const loading = document.querySelector('#splash .loading');
+    const startButton = document.querySelector('#splash .start-button');
+
+    const emitEvent = (eventName, listeners) => {
+        listeners.forEach((listener) => {
+            const el = document.querySelector(listener);
+            el.emit(eventName);
+        })
+    };
+
+    scene.addEventListener('loaded', function (e) {
+        setTimeout(() => {
+            loading.style.display = 'none';
+            splash.style.backgroundColor = 'rgba(0, 0, 0, 0.85)';
+            startButton.style.opacity = 1;
+        }, 50);
+    });
+
+    startButton.addEventListener('click', function (e) {
+        splash.style.display = 'none';
+        emitEvent('scene-started', ['#planet', '#shuttle-light', '#shuttle-light', '#shuttle', '#shuttle']);
+    });
+});
