d408ed2709310694ba6e50af1b4824c260a6f1c2
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-11-11 18:07:31 +0100

    dd

---
+++ Documentacio/layer/toggle-layer.js
@@ -0,0 +1,12 @@
+AFRAME.registerComponent('toggle-layer', {
+
+  init: function () {
+
+    var layerEl = document.querySelector('[layer]');
+    this.el.addEventListener('thumbstickdown', function () {
+      layerEl.components.layer.toggleCompositorLayer();
+    });
+
+  }
+
+});
