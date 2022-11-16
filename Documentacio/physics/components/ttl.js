a006a50abc9be7a63cbe4fb7fd26c5bc9b80ee4a
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-11-16 13:22:54 +0100

    dd

---
+++ Documentacio/physics/components/ttl.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent('ttl', {
+    schema: {
+        time: {type: 'number', default: 100}
+    },
+
+    init: function(){
+        this.ticks = 0;
+        this.lifetime = this.data.time;
+    },
+
+    tick: function(){
+        this.lifetime--;
+        if(this.lifetime === 0){
+
+            // TTL ha expirat, llavor eliminar l'entitat
+            this.el.sceneEl.removeChild(this.el);
+        }
+    }
+
+});
